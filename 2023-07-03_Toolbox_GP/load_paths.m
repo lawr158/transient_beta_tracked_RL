616fd4acddefe30bb1a034e7ae57b592f20b7281
Law <CH235520@CHBOSTON.ORG>
2023-10-11 13:28:50 -0400

    updated load_paths.m

--- 2023-07-03_Toolbox_GP/load_paths.m
+++ 2023-07-03_Toolbox_GP/load_paths.m
@@ -1,4 +1,12 @@
 function path = load_paths()
     % helper paths for below
-    path = load_paths_gp_abcct;
+    path.rcfs = '';
+    path.eeg = '';
+    % import paths
+    % path to beapp files
+    path.beapp_files = '';
+    % BEAPP run tag
+    path.beapp_tag = '';
+    % desired path to output tables to
+    path.out_tables = '';
 end
